7bc5c35124f8a571af00a0cad1df5e2b1f519361
KeytechFileServer <docs.keytech@gmail.com>
2019-12-05 10:36:59 +0900

    1차 수정 완료

--- gitTestProject/src/main/java/gitTestProject/Library.java
+++ gitTestProject/src/main/java/gitTestProject/Library.java
@@ -6,6 +6,7 @@ package gitTestProject;
 public class Library {
 	public static void main(String[] args) {
 		System.out.println("____________________________");
+		System.out.println("############################");
 	}
     public boolean someLibraryMethod() {
         return true;
